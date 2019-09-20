beaadb18cae22a52022c1e146c0cf22142c24ad4
bendiller <diller.benjamin@gmail.com>
2019-09-20 10:42:21 -0600

    Successful selector developed for future meetings.

--- selector_sandbox.py
+++ selector_sandbox.py
@@ -3,29 +3,34 @@ without involving the rest of scrapy."""
 
 from scrapy.selector import Selector
 
-with open("FULL_SOURCE.html", 'r') as f:
+with open("source.html", 'r') as f:
     body = f.read()  # f.readlines()
 
-selector_str = "//div/ul/li"
-# selector_str = "//div"
+# selector_str = "//div/div/div/ul/li"  # Way too many results.
 
-# print(body)
+# This correctly selects only the two headers that I care about, but not the ULs adjacent (which contain meeting data)
+# selector_str = "//div/div/div/h3"
 
-# if 'div' in body:
-#     print('Hmm')
-# else:
-#     print('Wtf')
+# This selects the first UL I care about, but also 2 others I don't, and misses one that I do want.
+selector_str = "//div/div/div/h3/following-sibling::ul"
 
-#
-# for item in body:
-#     print(item)
+# Either an extract_first() or narrowing down further might work.
+first_result = Selector(text=body).xpath(selector_str).extract_first()
 
-# print(type(body))
-# print(Selector(text=body).xpath(selector_str).get())
-
-
-# body = '<html><body><span>good</span></body></html>'
-# selector_str = '//span/text()'
-
-for item in Selector(text=body).xpath(selector_str):
+for item in Selector(text=first_result).xpath('//li'):
     print(item.extract())
+    # This can then be passed over to it's own special parser. I can drop those which are in the past - those will be
+    # present in the next section (with the "Commission Meetings" header, that details past meetings).
+
+
+
+
+
+
+
+
+
+# print(f"Number of items matching selector: {len(Selector(text=body).xpath(selector_str))}")
+#
+# for item in Selector(text=body).xpath(selector_str):
+#     print(item.extract())
